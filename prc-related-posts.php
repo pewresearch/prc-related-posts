8387f94fdfa03e35403f6103d5e30d0c48f8eae9
prcdevgitbot <webdev@pewresearch.org>
2026-07-21 14:54:51 +0000

    See https://github.com/pewresearch/prc-platform/commit/b18ceb7c8c0a631b919e50de2a0e1b2dc88cd93d from refs/tags/v1.12.0

--- prc-related-posts.php
+++ prc-related-posts.php
@@ -31,10 +31,6 @@ if ( ! defined( 'WPINC' ) ) {
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-if ( ! defined( 'DEFAULT_TECHNICAL_CONTACT' ) ) {
-	define( 'DEFAULT_TECHNICAL_CONTACT', 'webdev@pewresearch.org' );
-}
-
 define( 'PRC_RELATED_POSTS_FILE', __FILE__ );
 define( 'PRC_RELATED_POSTS_DIR', __DIR__ );
 define( 'PRC_RELATED_POSTS_BLOCKS_DIR', __DIR__ . '/build' );
@@ -49,27 +45,6 @@ if ( ! defined( 'PRC_PLATFORM' ) ) {
 	}
 	unset( $prc_related_posts_autoloader );
 }
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-activator.php
- */
-function activate() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-activator.php';
-	Plugin_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-deactivator.php
- */
-function deactivate() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-deactivator.php';
-	Plugin_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, '\PRC\Platform\Related_Posts\activate' );
-register_deactivation_hook( __FILE__, '\PRC\Platform\Related_Posts\deactivate' );
 
 /**
  * Helper utilities
